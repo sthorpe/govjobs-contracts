77346b5e0c3885b5315a6294a1706d62aeeda583
Scott Thorpe <sthorpe@gmail.com>
2018-07-27 19:41:02 -0400

    Adding skeleton for our smart contract

---
+++ contracts/GovJobs.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.23;
+
+
+contract GovJobs {
+    uint storedData;
+
+    function voteJob() public {
+    }
+
+    function activeJobs() public {
+    }
+
+    function agency() public {
+    }
+
+    function projects() public {
+    }
+
+    function setupDownloadLink() public {
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
